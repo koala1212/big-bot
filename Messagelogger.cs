903f0fd169e9a34e8d2e3d1d0f58daafb75608e8
koala1212 <50882653+koala1212@users.noreply.github.com>
2022-08-06 18:12:51 -0500

    Add files via upload

---
+++ Messagelogger.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using discord_bot;
+using Discord;
+
+namespace discord_bot;
+
+public class Info : Program
+
+{
+    public async Task<(List<Message> records, ulong fromMessageId)> Getmessageinfo(ulong fromMessageId,
+        IMessageChannel channel, bool order)
+    {
+        var records = new List<Message> { };
+        IEnumerable<IMessage> messages;
+        if (order == true)
+        {
+            messages = await channel.GetMessagesAsync(fromMessageId: fromMessageId, dir: Direction.Before)
+                .FlattenAsync();
+        }
+        else
+        {
+            messages = await channel.GetMessagesAsync(1)
+                .FlattenAsync();
+        }
+
+        await Task.Delay(500);
+        int i = 0;
+        foreach (var message in messages)
+        {
+            fromMessageId = message.Id;
+            var author = message.Author;
+            var timestamp = message.Timestamp;
+            var attachments = message.Attachments;
+            var attachmentlist = "";
+            List<string> atList = new List<string>();
+            foreach (var attachment in attachments)
+            {
+                atList.Add(attachment.ProxyUrl);
+            }
+
+            foreach (var attachment in atList)
+            {
+                attachmentlist = attachmentlist + "\n" + attachment + ",";
+            }
+
+            records.Add(new Message
+            {
+                _userID = author.Discriminator,
+                _author = author.Username,
+                _timestamp = timestamp,
+                _content = message.Content,
+                _attachment = attachmentlist
+            });
+            Console.WriteLine(
+                $"User:{records[i]._author} Timestamp:{records[i]._timestamp} Message: {records[i]._content} ");
+
+            i++;
+        }
+
+
+        return (records, fromMessageId);
+    }
+}
---
+++ ProgramPublic.cs
@@ -0,0 +1,83 @@
+﻿using System.Globalization;
+using CsvHelper;
+using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+
+namespace discord_bot;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private DiscordSocketClient _client;
+
+    public class Message
+    {
+        public string _author { get; set; }
+        public string _userID { get; set; }
+        public DateTimeOffset _timestamp { get; set; }
+        public string _attachment { get; set; }
+        public string _content { get; set; }
+    }
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+
+
+        var token = "TYPE_BOT_TOKEN_HERE";
+
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        _client.Ready += _client_Ready;
+        Console.WriteLine(_client.ConnectionState);
+
+
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    private async Task _client_Ready()
+    {
+        var channel = _client.GetChannel(CHANNEL ID HERE) as IMessageChannel;
+        ulong fromMessageId = 0;
+
+        var frommessage = channel.GetMessagesAsync(1).FlattenAsync().Result;
+
+        var records = new List<Message> { };
+        foreach (var message in frommessage)
+        {
+            fromMessageId = message.Id;
+            break;
+        }
+
+        await new Info().Getmessageinfo(fromMessageId, channel, false);
+        bool go = true;
+        while (go)
+        {
+            try
+            {
+                var output = await new Info().Getmessageinfo(fromMessageId, channel, true);
+                fromMessageId = output.fromMessageId;
+                foreach (var outputRecord in output.records) records.Add(outputRecord);
+
+                await using var writer = new StreamWriter("PATH TO CSV FILE");
+                await using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+                await csv.WriteRecordsAsync(records);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message, Console.BackgroundColor == ConsoleColor.DarkRed);
+                go = false;
+            }
+        }
+    }
+}
